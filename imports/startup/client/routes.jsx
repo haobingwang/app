4f36dbc63beedc7faf5d48db66a91fd94ec5b73a
hobartwang <hobart.cn@gmail.com>
2016-04-13 17:59:06 +0800

    添加整体页面框架

---
+++ imports/startup/client/routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+
+// route componets
+import App from '../../ui/pages/App.jsx';
+import SignIn from '../../ui/pages/SignIn.jsx';
+import Calculator from '../../ui/pages/Calculator.jsx';
+import Blog from '../../ui/pages/Blog.jsx';
+import TodoList from '../../ui/pages/TodoList.jsx';
+import NotFoundPage from '../../ui/pages/NotFoundPage.jsx';
+
+export const renderRoutes = () => (
+	<Router history={browserHistory}>
+		<Route path="/" component={App}></Route>
+		<Route path="signin" component={SignIn}/>
+		<Route path="calcu" component={Calculator}/>
+		<Route path="blog" component={Blog}/>
+		<Route path="*" component={NotFoundPage}/>
+	</Router>
+);
